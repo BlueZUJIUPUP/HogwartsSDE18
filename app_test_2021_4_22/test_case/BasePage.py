ad67e90a487b774e284bd5706e38cae17202fd01
z1003033614@163.com <z1003033614@163.com>
2021-04-23 01:10:21 +0800

    appium作业练习1

---
+++ app_test_2021_4_22/test_case/BasePage.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @File   : test_one
+# @Time   : 2021/4/22 10:28
+# @Author : BLUE_JUZIUPUP
+
+
+class BasePage:
+
+    pass
---
+++ app_test_2021_4_22/test_case/app_contact.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @File   : app_contact
+# @Time   : 2021/4/22 22:49 
+# @Author : BLUE_JUZIUPUP
