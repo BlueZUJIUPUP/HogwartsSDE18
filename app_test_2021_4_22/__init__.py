24babc18c5924cedee2a5cf981de4357e4c3228a
z1003033614@163.com <z1003033614@163.com>
2021-04-22 11:55:31 +0800

    appium上手练习

---
+++ app_test_2021_4_22/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @File   : __init__.py
+# @Time   : 2021/4/22 10:26 
+# @Author : BLUE_JUZIUPUP
---
+++ app_test_2021_4_22/test_case/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @File   : __init__.py
+# @Time   : 2021/4/22 10:27 
+# @Author : BLUE_JUZIUPUP
