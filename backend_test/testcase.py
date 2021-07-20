eb5dcef751dab4c2c3502ec6cdab7f28eff77ae6
z1003033614@163.com <z1003033614@163.com>
2021-07-20 11:58:28 +0800

    'ui布局完成'

--- backend_test/testcase.py
+++ backend_test/testcase.py
@@ -15,14 +15,14 @@ class Testcase:
         assert r.status_code == 200
 
     def test_get2(self):
-        data = {"id" : 4}
-        r = requests.get(url=self.url,params=data)
+        data = {"id": 4}
+        r = requests.get(url=self.url, params=data)
         print(r.text)
         assert r.status_code == 200
 
     def test_post(self):
-        data = {"id" : 5, 'nodeid' : 5, 'remark' : 5}
-        r = requests.post(url=self.url,json=data)
+        data = {"id": 5, 'nodeid': 5, 'remark': 5}
+        r = requests.post(url=self.url, json=data)
         assert r.status_code == 200
 
     def test_put(self):
@@ -32,11 +32,36 @@ class Testcase:
 
     def test_put2(self):
         data = {"id": 2, 'nodeid': '12', 'remark': '12'}
-        r = requests.put(url=self.url,json=data)
+        r = requests.put(url=self.url, json=data)
         assert r.status_code == 200
         print(r.text)
 
     def test_delete(self):
         data = {"id": 4}
-        r = requests.delete(url=self.url,params=data)
+        r = requests.delete(url=self.url, params=data)
         print(r.text)
+
+    def test_01(self):
+        headers = {"Content-Type": "application/json"}
+        data = {"email": "1003033614@qq.com",
+                "password": "zyy123",
+                "userName": "zyy1233"}
+        r = requests.post("http://stuq.ceshiren.com:8089/user/register", json=data, headers=headers)
+        print(r.text)
+
+    def test_02(self):
+        headers = {"Content-Type": "application/json"}
+        data = {"password": "zyy123",
+                "userName": "zyy1233"}
+        r = requests.post("http://stuq.ceshiren.com:8089/user/login", json=data, headers=headers)
+        print(r.text)
+
+    def test_03(self):
+        headers = {
+            "Content-Type": "application/json",
+            "token":"f762e83f8662e7985aa5cb557a2dd026"
+            }
+        data = {}
+        r = requests.get("http://stuq.ceshiren.com:8089/user/isLogin", headers=headers)
+        print(r.text)
+
